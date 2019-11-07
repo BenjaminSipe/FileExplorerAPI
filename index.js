3d051b4a11ef3a560a590d538c2567e8f80460fa
BenjaminSipe <benjaminmsipe@student.cofo.edu>
2019-11-07 11:23:08 -0600

    commit

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 var fs = require('fs');
- 
+
 app.put('/api/mkdir', (req, res) => {
     var path = req.query.path;
-    if (path == null ) res.send("No path specified in query.");
+    if (path == null) res.send("No path specified in query.");
     var name = req.query.name;
-    if (name == null ) res.send("No name specified in query.");
-    fs.mkdir(path+"/"+name,(err) => {
+    if (name == null) res.send("No name specified in query.");
+    fs.mkdir(path + "/" + name, (err) => {
         if (err) res.send(err);
         else res.send("success")
     });
@@ -17,17 +17,18 @@ app.put('/api/mkdir', (req, res) => {
 app.put('/api/mkfile', (req, res) => {
     console.log(req.query)
     var path = req.query.path;
-    if (path == null ) res.send("No path specified in query.");
+    if (path == null) res.send("No path specified in query.");
     var name = req.query.name;
-    if (name == null ) res.send("No name specified in query.");
-    fs.open(path+"/"+name,"wx",(err, fd) => {
+    if (name == null) res.send("No name specified in query.");
+    fs.open(path + "/" + name, "wx", (err, fd) => {
         if (err) res.send(err);
         else {
             fs.close(fd, (err) => {
                 if (err) console.log(err)
                 res.send("success")
             })
-        }});
+        }
+    });
 })
 
 app.all('/api', (req, res) => {
@@ -44,26 +45,31 @@ app.get('/api/ls', async (req, res) => {
     } else {
         path = req.query.path;
     }
-    fs.readdir(path, async function(err, items) {
-        // console.log(items)
-        for (const item of items) {
-    
-            await fs.stat(path + "/" + item, async function(err, stats) {
-                numOfItems++
-                returnInfo.push({
-                    name:item,
-                    size:stats["size"],
-                    isDirectory:stats.isDirectory(),
-                    permissions:stats["mode"],
-                })
-                if (numOfItems == items.length) {
-                    res.send(JSON.stringify(await returnInfo)) 
-                }
-            });
-        }
+    fs.readdir(path, async function (err, items) {
+        console.log(items)
+        if (items.length == 0) {
+            res.send("[]")
+        } else {
 
+
+            for (const item of items) {
+
+                await fs.stat(path + "/" + item, async function (err, stats) {
+                    numOfItems++
+                    returnInfo.push({
+                        name: item,
+                        size: stats["size"],
+                        isDirectory: stats.isDirectory(),
+                        permissions: stats["mode"],
+                    })
+                    if (numOfItems == items.length) {
+                        res.send(JSON.stringify(await returnInfo))
+                    }
+                });
+            }
+        }
     });
 })
 //rename file
 
-app.listen(port, () => {console.log(`Example app listening on port ${port}!`)})
+app.listen(port, () => { console.log(`Example app listening on port ${port}!`) })
